7ae1cc710869429c17cf3b51f2ee8a6f9173110e
Roi Avinoam <avinoamr@gmail.com>
2017-06-04 21:33:00 +0300

    Test the new API

--- srv_test.go
+++ srv_test.go
@@ -1,49 +1,63 @@
 package postgressrv
 
 import (
-    // "fmt"
-    // "net"
+    "io"
+    "fmt"
+    "net"
     "testing"
-    // "database/sql"
+    "database/sql"
+    "database/sql/driver"
 
 	_ "github.com/lib/pq"
 )
 
 func TestOne(t *testing.T) {
-    // s := New(&MockQueryer{
-        // Cols: []Column{&MockColumn{"roi", 0}},
-        // Rows: [][]string{
-        //     []string{"one"},
-        //     []string{"two"},
-        // },
-    // })
+    mock := &rows{}
+    mock.AddCol("one")
+    mock.AddRow("hello")
+    mock.AddRow("world")
 
-    // go func() {
-    //     ln, err := net.Listen("tcp", ":5432")
-    //     if err != nil {
-    //         t.Fatal(err)
-    //     }
-    //     conn, err := ln.Accept()
-    //     if err != nil {
-    //         t.Fatal(err)
-    //     }
-    //
-    //     fmt.Println("RECEIVED CONN")
-    //     err = s.Serve(conn)
-    //     if err != nil {
-    //         t.Fatal(err)
-    //     }
-    // }()
-    //
-    // db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=disable")
-    // if err != nil {
-    //     t.Fatal(err)
-    // }
-    //
-    // rows, err := db.Query("SELECT 1")
-    // if err != nil {
-    //     t.Fatal(err)
-    // }
+    s := New(&queryer{mock})
+
+    go func() {
+        ln, err := net.Listen("tcp", ":5432")
+        if err != nil {
+            t.Fatal(err)
+        }
+        conn, err := ln.Accept()
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        fmt.Println("RECEIVED CONN")
+        err = s.Serve(conn)
+        if err != nil {
+            t.Fatal(err)
+        }
+    }()
+
+    db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=disable")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rows, err := db.Query("SELECT 1")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    fmt.Println(rows.Columns())
+
+    for rows.Next() {
+        var v string
+        err := rows.Scan(&v)
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        fmt.Println(v)
+    }
+
     //
     // cols, err := rows.Columns()
     // if err != nil {
@@ -52,33 +66,47 @@ func TestOne(t *testing.T) {
     // t.Logf("ROWS %v", cols)
 }
 
-// type MockQueryer struct {
-//     Cols []Column
-//     Rows [][]string
-// }
+type queryer struct { rows driver.Rows }
+func (m *queryer) Query(q string, args []driver.Value) (driver.Rows, error) {
+    return m.rows, nil
+}
 
-// func (m *MockQueryer) Query(q Query) error {
-//     fmt.Println("QUERY RECEIVED")
-//     err := q.WriteColumns(m.Cols...)
-//     if err != nil {
-//         return err
-//     }
-//
-//     for _, row := range m.Rows {
-//         cells := make([][]byte, len(row))
-//         for i, cell := range row {
-//             cells[i] = []byte(cell)
-//         }
-//
-//         err := q.WriteRow(cells...)
-//         if err != nil {
-//             return err
-//         }
-//     }
-//
-//     return nil
-// }
+type rows struct {
+    cols []string
+    rows [][]driver.Value
+}
 
-// type MockColumn struct { name string; oid uint }
-// func (c *MockColumn) Name() string { return c.name }
-// func (c *MockColumn) TypeOid() uint { return c.oid }
+func (*rows) Close() error {
+    panic("not implemented")
+}
+
+func (rows *rows) Columns() []string {
+    return rows.cols
+}
+
+func (rows *rows) Next(dest []driver.Value) error {
+    if len(rows.rows) == 0 {
+        return io.EOF
+    }
+
+    for i, v := range rows.rows[0] {
+        dest[i] = v
+    }
+
+    rows.rows = rows.rows[1:]
+    return nil
+}
+
+func (rows *rows) AddCol(name string) {
+    rows.cols = append(rows.cols, name)
+}
+
+func (rows *rows) AddRow(v string) {
+    var err error
+    row := make([]driver.Value, 1)
+    row[0], err = driver.String.ConvertValue(v)
+    if err != nil {
+        panic(err)
+    }
+    rows.rows = append(rows.rows, row)
+}
