3ad1e0b734b6e95af6cf612d6489c1774d61230d
Roi Avinoam <avinoamr@gmail.com>
2017-06-17 23:19:22 +0300

    Single Err interface

--- pgsrv.go
+++ pgsrv.go
@@ -2,7 +2,6 @@ package pgsrv
 
 import (
     "net"
-    "fmt"
     "context"
     "database/sql/driver"
 )
@@ -25,40 +24,4 @@ type Server interface {
 
     // Manually serve a connection
     Serve(net.Conn) error
-}
-
-// Error object that includes a hint text
-type ErrHinter interface {
-    error
-    Hint() string
-}
-
-// Error object that includes an error code
-// See list of available error codes here:
-//      https://www.postgresql.org/docs/10/static/errcodes-appendix.html
-type ErrCoder interface {
-    error
-    Code() string
-}
-
-// Undefined indicates that a certain entity (function, column, etc.) is not
-// registered or available for use.
-func Undefined(msg string, args ...interface{}) error {
-    return fmt.Errorf("Undefined " + msg, args...)
-}
-
-// Invalid indicates that the user request is invalid or otherwise incorrect.
-// It's very much similar to a syntax error, except that the invalidity is
-// logical within the request rather than syntactic. For example, using a non-
-// boolean expression in WHERE
-func Invalid(msg string, args ...interface{}) error {
-    return fmt.Errorf("Invalid " + msg, args...)
-}
-
-// Unsupported indicates that a certain feature is not supported. Unlike
-// Undefined - this error is not for cases where a user-space entity is not
-// recognized but when the recognized entity cannot perform some of its
-// functionality
-func Unsupported(msg string, args ...interface{}) error {
-    return fmt.Errorf("Unsupported " + msg, args...)
 }
