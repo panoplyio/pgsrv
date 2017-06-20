f8fa245ea17422ea32fe588d836408a29cf80545
Roi Avinoam <avinoamr@gmail.com>
2017-06-21 01:40:34 +0300

    Separate the default spec-based command complete tag as a separate helper struct, instead of in the main function for brevity and clarity

--- query.go
+++ query.go
@@ -19,53 +19,40 @@ type column struct {
     name string
 }
 
-// implements Execer
-func (q *query) Exec(ctx context.Context, n nodes.Node) error {
-    res, err := q.session.Exec(ctx, n)
+// Run the query using the Server's defined queryer
+func (q *query) Run() error {
+
+    // parse the query
+    ast, err := parser.Parse(q.sql)
     if err != nil {
         return q.session.Write(errMsg(err))
     }
 
-    var tag string
-    tagger, ok := res.(ResultTag)
-    if ok {
-        // tag is supplied by the results
-        tag, err = tagger.Tag()
+    // add the session to the context, cast to the Session interface just for
+    // compile time verification that the interface is implemented.
+    ctx := context.Background()
+    ctx = context.WithValue(ctx, "Session", Session(q.session))
+    ctx = context.WithValue(ctx, "SQL", q.sql)
+    ctx = context.WithValue(ctx, "AST", ast)
+
+    // execute all of the statements
+    for _, stmt := range ast.Statements {
+
+        // determine if it's a query or command
+        switch stmt.(type) {
+            case nodes.VariableShowStmt:    err = q.Query(ctx, stmt)
+            case nodes.SelectStmt:          err = q.Query(ctx, stmt)
+            default:                        err = q.Exec(ctx, stmt)
+        }
+
         if err != nil {
             return q.session.Write(errMsg(err))
         }
-    } else {
-        // default tag behavior to follow the spec described in CommandComplete:
-        // https://www.postgresql.org/docs/10/static/protocol-message-formats.html
-        affected, err := res.RowsAffected()
-        if err != nil {
-            return q.session.Write(errMsg(err))
-        }
-
-        switch n.(type) {
-        case nodes.CreateTableAsStmt:
-            tag = "SELECT" // follows the spec
-        case nodes.InsertStmt:
-            tag = "INSERT 0" // oid is not implemented; defaults to 0.
-        case nodes.DeleteStmt:
-            tag = "DELETE"
-        case nodes.FetchStmt:
-            tag = "FETCH"
-        case nodes.CopyStmt:
-            tag = "COPY"
-        case nodes.UpdateStmt:
-            tag = "UPDATE"
-        default:
-            tag = "UPDATE"
-        }
-
-        tag = fmt.Sprintf("%s %d", tag, affected)
     }
 
-    return q.session.Write(completeMsg(tag))
+    return nil
 }
 
-// implements Queryer
 func (q *query) Query(ctx context.Context, n nodes.Node) error {
     rows, err := q.session.Query(ctx, n)
     if err != nil {
@@ -111,39 +98,47 @@ func (q *query) Query(ctx context.Context, n nodes.Node) error {
     return q.session.Write(completeMsg(tag))
 }
 
-// Run the query using the Server's defined queryer
-func (q *query) Run() error {
-
-    // parse the query
-    ast, err := parser.Parse(q.sql)
+func (q *query) Exec(ctx context.Context, n nodes.Node) error {
+    res, err := q.session.Exec(ctx, n)
     if err != nil {
         return q.session.Write(errMsg(err))
     }
 
-    // add the session to the context, cast to the Session interface just for
-    // compile time verification that the interface is implemented.
-    ctx := context.Background()
-    ctx = context.WithValue(ctx, "Session", Session(q.session))
-    ctx = context.WithValue(ctx, "SQL", q.sql)
-    ctx = context.WithValue(ctx, "AST", ast)
-
-    // execute all of the statements
-    for _, stmt := range ast.Statements {
-
-        // determine if it's a query or command
-        switch stmt.(type) {
-        case nodes.VariableShowStmt:
-            err = q.Query(ctx, stmt)
-        case nodes.SelectStmt:
-            err = q.Query(ctx, stmt)
-        default:
-            err = q.Exec(ctx, stmt)
-        }
-
-        if err != nil {
-            return q.session.Write(errMsg(err))
-        }
+    t, ok := res.(ResultTag)
+    if !ok {
+        t = &tagger{res, n}
     }
 
-    return nil
+    tag, err := t.Tag()
+    if err != nil {
+        return q.session.Write(errMsg(err))
+    }
+
+    return q.session.Write(completeMsg(tag))
+}
+
+// implements the CommandComplete tag according to the spec as described at the
+// link below. When there's no suitable tag according to the spec, "UPDATE" is
+// used instead.
+// https://www.postgresql.org/docs/10/static/protocol-message-formats.html
+type tagger struct { driver.Result; Node nodes.Node }
+func (res *tagger) Tag() (string, error) {
+    affected, err := res.RowsAffected()
+    if err != nil {
+        return "", err
+    }
+
+    var tag string
+    switch res.Node.(type) {
+        // oid in INSERT is not implemented; defaults to 0
+        case nodes.InsertStmt:          tag = "INSERT 0"
+        case nodes.CreateTableAsStmt:   tag = "SELECT" // follows the spec
+        case nodes.DeleteStmt:          tag = "DELETE"
+        case nodes.FetchStmt:           tag = "FETCH"
+        case nodes.CopyStmt:            tag = "COPY"
+        case nodes.UpdateStmt:          tag = "UPDATE"
+        default:                        tag = "UPDATE"
+    }
+
+    return fmt.Sprintf("%s %d", tag, affected), nil
 }
