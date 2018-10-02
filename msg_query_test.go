2da0dd1730ffa6e651b8999e092a58363b565994
yury <yury@panoply.io>
2018-10-02 18:18:21 +0300

    Add unit test for completeMsg function

--- msg_query_test.go
+++ msg_query_test.go
@@ -9,3 +9,15 @@ func TestReadyMsg(t *testing.T) {
 	msg := readyMsg()
 	require.Equal(t, []byte{'Z', 0, 0, 0, 5, 'I'}, []byte(msg))
 }
+
+func TestCompleteMsg(t *testing.T) {
+	msg := completeMsg("meh")
+	expectedMsg := []byte{
+		'C',        // type
+		0, 0, 0, 8, // size
+		109, 101, 104, // meh in bytes
+		0, // null terminator
+	}
+
+	require.Equal(t, expectedMsg, []byte(msg))
+}
